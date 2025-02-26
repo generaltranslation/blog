6b91fed0c7e1f01b4b96e207911f08d4a5b29862
Brian Lou <brianlou2002@gmail.com>
2025-02-25 19:11:37 -0800

    Move stuffs

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
-import { redirect } from 'next/navigation'
+import { sortPosts, allCoreContent } from 'pliny/utils/contentlayer'
+import { allBlogs } from 'contentlayer/generated'
+import Main from './Main'
 
-export default function Page() {
-  return redirect('/blog')
+export default async function Page() {
+  const sortedPosts = sortPosts(allBlogs)
+  const posts = allCoreContent(sortedPosts)
+  return <Main posts={posts} />
 }
