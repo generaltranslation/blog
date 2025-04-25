ee10a7b74bf6f044c321fb39039a4cd2894110d2
archie-mckenzie <archie@generaltranslation.com>
2025-04-25 13:14:42 -0700

    dashboard

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,6 +1,6 @@
 const headerNavLinks = [
   { href: '/', title: 'Home' },
-  { href: 'https://generaltranslation.com', title: 'Dashboard' },
+  { href: 'https://dash.generaltranslation.com', title: 'Dashboard' },
   { href: 'https://generaltranslation.com/docs', title: 'Docs' },
   { href: 'https://generaltranslation.com/pricing', title: 'Pricing' },
   { href: '/about', title: 'About' },
